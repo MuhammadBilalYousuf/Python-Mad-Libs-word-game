bc29ea6a4d9fd9f810f9d2a4927435813f4f8c02
Bilal yousuf <bilal.yousuf@mobiwhiz.com>
2022-05-10 14:32:11 +0500

    first commit

---
+++ index.py
@@ -0,0 +1,6 @@
+from template import corefile
+import random
+
+if __name__ == "__main__":
+    m = random.choice([corefile])
+    m.file()
---
+++ template/corefile.py
@@ -0,0 +1,7 @@
+def file():
+    adj1 = input("Adjective: ")
+    adj2 = input("Adjective: ")
+
+    file = f"A {adj1} test content {adj2}."
+
+    print(file)
